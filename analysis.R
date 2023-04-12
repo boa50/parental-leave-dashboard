cef32e8abceddaba500427513bbd204edbb16e0e
boa50 <bruno.albuquerque50@gmail.com>
2023-04-13 04:53:00 +0930

    Prepared the data to be used during the analysis

--- analysis.R
+++ analysis.R
@@ -1,11 +1,17 @@
 library(vroom)
+library(janitor)
+library(dplyr)
+library(ggplot2)
 
 df <- vroom("data/parental_leave.csv", 
             col_select = 1:6, 
-            col_types = cols("c", "c", "i", "i", "i", "i"),
-            na = 0)
-
-df[is.na(df)] <- 0
+            col_types = cols("c", "c", "i", "i", "i", "i")) %>% 
+  clean_names() %>% 
+  mutate_if(is.integer, ~replace(., is.na(.), 0)) %>% 
+  mutate(
+    total_maternity_leave = paid_maternity_leave + unpaid_maternity_leave,
+    total_paternity_leave = paid_paternity_leave + unpaid_paternity_leave
+  )
 
 # Companies with more and less parental leave (total, separated by gender)
 # Maybe a distribution (histogram) chart
