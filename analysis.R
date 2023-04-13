f51c2c6eeedd5ec998e0d89540ab99c953d77981
boa50 <bruno.albuquerque50@gmail.com>
2023-04-14 05:10:50 +0930

    Created a sample chart to demonstrate the share between men and women work leave

--- analysis.R
+++ analysis.R
@@ -46,40 +46,34 @@ df <- vroom("data/parental_leave.csv",
 # Companies with more and less parental leave (total, separated by gender)
 # Maybe a distribution (histogram) chart
 # Create data to be used by the geom_density
-
-df %>% 
+df_test <- df %>% 
   group_by(total_leave) %>% 
   summarise(qty = n(),
             women_time = mean(total_maternity_leave)) %>% 
   mutate(men_time = total_leave - women_time,
-         pct_women = women_time / total_leave,
-         pct_men = men_time / total_leave,
+         pct_women = women_time / ifelse(total_leave == 0, 1, total_leave),
+         pct_men = men_time / ifelse(total_leave == 0, 1, total_leave),
          share_women = pct_women * qty,
          share_men = pct_men * qty) %>% 
-  pivot_longer(c(share_women, share_men)) %>% 
-  filter(!is.na(value)) %>% 
+  pivot_longer(c(share_women, share_men)) %>%
+  mutate_if(is.numeric, ~replace_na(., 0)) %>%
+  select(total_leave, name, value)
+
+generate_values <- function(i, tb) {
+  tb_temp <- tibble(
+    total_leave = df_test[i,]$total_leave,
+    type_leave = rep(df_test[i,]$name, ceiling(df_test[i,]$value))
+  )
+  
+  rbind(tb, tb_temp)
+}
+
+tb3 <- tibble()
+tb3 <- purrr::map_df(1:nrow(df_test), generate_values, tb = tb3)
+
+tb3 %>% 
   ggplot(aes(x = total_leave)) + 
-  geom_density(aes(fill = name), position = "stack")
-  # geom_area(aes(fill = name), position = "stack")
-
-df %>% 
-  ggplot(aes(x = total_leave, y = value)) + 
-  # geom_histogram() +
-  geom_density()
-
-df %>% 
-  filter(total_paternity_leave > 0) %>% 
-  pivot_longer(c(total_maternity_leave, total_paternity_leave)) %>% 
-  group_by(name, value) %>% 
-  summarise(total = n()) %>% 
-  ggplot(aes(x = value, y = total)) + 
-  # geom_histogram() +
-  geom_area(aes(fill = name), position = "stack")
-
-df %>% 
-  pivot_longer(c(total_maternity_leave, total_paternity_leave)) %>% 
-  group_by(company, name, value) %>% 
-  summarise(total_per_company = sum(value))
+  geom_density(aes(fill = type_leave), position = "stack")
   
 
 # Cards with best and worst companies depending on filters
